50809d13c8531fbc392c7aeab60f831ab2bf44ca
Christopher Ouellette <chrisryanouellette@gmail.com>
2023-11-13 18:37:22 -0500

    fix: .npmrc file

--- packages/bin/npmrc/src/index.ts
+++ packages/bin/npmrc/src/index.ts
@@ -1,23 +1,16 @@
 import * as fsPromises from "fs/promises";
 import * as path from "path";
+import * as url from "url";
 import { setFailed } from "@actions/core";
 
-const content = `//registry.npmjs.org/:_authToken=${process.env.token}
-registry=https://registry.npmjs.org/
-always-auth=true`;
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const root = path.join(__dirname, "../../../../.npmrc");
 
 try {
-  const maybePackagePaths = process.env.packages;
-  if (!maybePackagePaths) {
-    throw new Error(
-      "Package file path not provided. Check the packages action was ran before this action and the env var packages' is set.",
-    );
-  }
-  const workspacePaths = JSON.parse(maybePackagePaths);
-  for (const workspacePath of workspacePaths) {
-    const filePath = path.join(workspacePath, ".npmrc");
-    await fsPromises.writeFile(filePath, content);
-  }
+  const content = `//registry.npmjs.org/:_authToken=${process.env.token}
+registry=https://registry.npmjs.org/
+always-auth=true`;
+  await fsPromises.writeFile(root, content);
 } catch (error) {
   if (error instanceof Error) {
     setFailed(error.message);
