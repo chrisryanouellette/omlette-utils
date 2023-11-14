c962856b4edaf7d3f275fe8f99e8c500438300f4
Chris O <35933106+chrisryanouellette@users.noreply.github.com>
2023-11-13 19:45:28 -0500

    fix: publish action (#6)
    * fix: publish action
    
    * test: log error
    
    * fix: package info command
    
    * chore: revert pipeline changes

--- packages/bin/versions/src/index.ts
+++ packages/bin/versions/src/index.ts
@@ -2,7 +2,7 @@ import * as fsPromises from "fs/promises";
 import * as childProcess from "child_process";
 import * as util from "util";
 import * as path from "path";
-import { setOutput, setFailed } from "@actions/core";
+import { setOutput, setFailed, info, error as logError } from "@actions/core";
 import * as semver from "semver";
 
 const exec = util.promisify(childProcess.exec);
@@ -16,12 +16,23 @@ const outputKey = "output";
 
 async function getRemoteNpmVersion(name: string): Promise<string | null> {
   try {
-    const response = await exec(`npm view ${name} version --json`);
+    const response = await exec(`yarn info ${name} version --json`);
     if (response.stderr) throw response.stderr;
-    return response.stdout;
+    return JSON.parse(response.stdout).data;
   } catch (error) {
+    if (error instanceof Error) {
+      logError(error);
+    } else {
+      logError(`Unknown Error: ${JSON.stringify(error)}`);
+    }
     return null;
   }
+}
+
+function isOutdated(a: string | null, b: string): boolean {
+  if (!a) return true;
+  if (a === b) return false;
+  return semver.lt(a, b);
 }
 
 try {
@@ -39,8 +50,11 @@ try {
       encoding: "utf-8",
     });
     const { name, version }: PackageJson = JSON.parse(file);
+    info(`Checking package "${name}"'s version`);
     const maybeVersion = await getRemoteNpmVersion(name);
-    if (!maybeVersion || semver.lt(maybeVersion, version)) {
+    info(!maybeVersion ? "No Version found" : `Version ${maybeVersion} found`);
+    if (isOutdated(maybeVersion, version)) {
+      info(`Package "${name}" is outdate.`);
       outdated.push(name);
     }
   }
