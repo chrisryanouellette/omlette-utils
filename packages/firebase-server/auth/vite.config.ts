3a2cecde83dbfeab2a4d697a3309577052eb7772
Christopher Ouellette <chrisryanouellette@gmail.com>
2023-11-26 17:56:01 -0500

    fix: external node modules

--- packages/firebase-server/auth/vite.config.ts
+++ packages/firebase-server/auth/vite.config.ts
@@ -8,19 +8,49 @@ const fileName = fileNameWithExt.split(".")[0];
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    target: "node20",
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: path.resolve(__dirname, "src/index.ts"),
       name: packageJson.name,
       // the proper extensions will be added
       fileName: fileName,
+      formats: ["cjs"],
     },
     rollupOptions: {
       external: [
-        "firebase",
-        "firebase/firestore",
-        "@ouellettec/utils-firebase",
+        "os",
+        "path",
+        "zlib",
+        "fs",
+        "stream",
+        "crypto",
+        "http",
+        "https",
+        "events",
+        "util",
+        "url",
+        "firebase-admin",
+        "firebase-admin/app",
+        "@ouellettec/utils-firebase-admin",
       ],
+      output: {
+        globals: {
+          os: "os",
+          path: "path",
+          zlib: "zlib",
+          fs: "fs",
+          stream: "stream",
+          crypto: "crypto",
+          http: "http",
+          https: "https",
+          events: "events",
+          util: "util",
+          url: "url",
+          "firebase-admin": "firebase-admin",
+          "firebase-admin/app": "firebase-admin/app",
+        },
+      },
     },
   },
 });
