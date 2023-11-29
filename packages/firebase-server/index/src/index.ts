8075cb14e1e5b96ae6a9718e4b8d0558ca4ef26a
Christopher Ouellette <chrisryanouellette@gmail.com>
2023-11-29 18:16:09 -0500

    fix: admin import

--- packages/firebase-server/index/src/index.ts
+++ packages/firebase-server/index/src/index.ts
@@ -1,4 +1,4 @@
-import * as admin from "firebase-admin";
+import admin from "firebase-admin";
 import { Throwable, isSSR } from "@ouellettec/utils";
 import { App as FirebaseAdminApp, getApps } from "firebase-admin/app";
 
